9be148733b1b7e3079c576a2591ee503601db623
Alan Falcon <falconswoosh@gmail.com>
2017-10-27 11:51:56 -0700

    Enter constructor for stylist in Salon.CS

--- Salon/Models/Salon.cs
+++ Salon/Models/Salon.cs
@@ -1,11 +1,48 @@
 using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Salon.Models
 {
-  public class Class
+  public class Salon
   {
+    //stylist info
+    private string _stylistLastName;
+    private string _stylistFirstName;
+    private int _id
+
+
+    public Salon(string stylistLastName, string stylistFirstName, int id = 0)
+    {
+        _stylistLastName = stylistLastName;
+        _stylistFirstName = stylistFirstName;
+        _id = id;
+    }
+
+    public override bool Equals(System.Object OtherSalon)
+    {
+      if (!(OtherSalon is Salon))
+      {
+        return false;
+      }
+      else
+      {
+        Salon newSalon = (Salon) other Salon;
+        bool idEquality = this.GetId() == newSalon.GetId();
+        bool stylistLastNameEquality = this.GetStylistLastName() == newSalon.GetStylistLastName();
+        bool stylistFirstNameEquality = this.GetStylistFirstName() == newSalon.GetStylistFirstName();
+      }
+    }
+    public override int GetHashCode()
+    {
+        return this.GetStylistLastName().GetHashCode();
+    }
+
+
+
+
+
     public static void ClearAll()
       {
         MySqlConnection conn = DB.Connection();
