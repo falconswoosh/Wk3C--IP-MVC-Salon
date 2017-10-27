c7b9d47f390f5714445d9b58fe9c789671a88737
Alan Falcon <falconswoosh@gmail.com>
2017-10-27 14:36:26 -0700

    add save method to salon.cs as well as first test to verify EqualOverride.

--- Salon.Tests/Models.Tests/Salon.Tests.cs
+++ Salon.Tests/Models.Tests/Salon.Tests.cs
@@ -7,16 +7,28 @@ using Salon.Models;
 namespace Salon.Models.Tests
 {
   [TestClass]
-  public class ClassTests : IDisposable
+  public class StylistTests : IDisposable
   {
-    public void Dispose()
+    public StylistTests()
     {
-      Class.ClearAll
+        DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=salon_test;";
     }
+    // public void Dispose()
+    // {
+    //   Class.ClearAll
+    // }
     [TestMethod]
-    public void Method_Description_ExpectedValue()
+    public void Equals_OverrideTrueForSameDescription_sLastName()
     {
-      Assert.AreEqual(var1, method(input));
+      Stylist FirstStylistLName = new Stylist("Susan", 1);
+      Stylist SecondStylistLName = new Stylist("Susan", 1);
+
+      Assert.AreEqual(FirstStylistLName, SecondStylistLName);
     }
+    // [TestMethod]
+    // public void Method_Description_ExpectedValue()
+    // {
+    //   Assert.AreEqual(var1, method(input));
+    // }
   }
 }
--- Salon/Models/Salon.cs
+++ Salon/Models/Salon.cs
@@ -5,58 +5,101 @@ using System.Text.RegularExpressions;
 
 namespace Salon.Models
 {
-  public class Salon
+  public class Stylist
   {
     //stylist info
-    private string _stylistLastName;
-    private string _stylistFirstName;
-    private int _id
+    private string _sLastName;
+    private string _sFirstName;
+    private int _sId
 
 
-    public Salon(string stylistLastName, string stylistFirstName, int id = 0)
+    public Stylist(string sLastName, string sFirstName, int sId = 0)
     {
-        _stylistLastName = stylistLastName;
-        _stylistFirstName = stylistFirstName;
-        _id = id;
+        _sLastName = sLastName;
+        _sFirstName = sFirstName;
+        _sId = sId;
     }
 
-    public override bool Equals(System.Object OtherSalon)
+    public override bool Equals(System.Object OtherStylist)
     {
-      if (!(OtherSalon is Salon))
+      if (!(OtherStylist is Stylist))
       {
         return false;
       }
       else
       {
-        Salon newSalon = (Salon) other Salon;
-        bool idEquality = this.GetId() == newSalon.GetId();
-        bool stylistLastNameEquality = this.GetStylistLastName() == newSalon.GetStylistLastName();
-        bool stylistFirstNameEquality = this.GetStylistFirstName() == newSalon.GetStylistFirstName();
+        Stylist newSalon = (Stylist) otherStylist;
+        bool idEquality = this.GetSId() == newStylist.GetSId();
+        bool sLastNameEquality = this.GetsLastName() == newStylist.GetsLastName();
+        bool sFirstNameEquality = this.GetsFirstName() == newStylist.GetsFirstName();
+        return (sidEquality && slastNameEquality && sFirstNameEquality);
       }
     }
     public override int GetHashCode()
     {
-        return this.GetStylistLastName().GetHashCode();
+        return this.GetsLastName().GetHashCode();
     }
+    public string GetsLastName()
+    {
+        return _sLastName;
+    }
+    public string GetsFirstName()
+    {
+        return sFirstName;
+    }
+    public int GetSId()
+    {
+        return _sId;
+    }
+//ADD:  ClientID here
 
-
-
-
-
-    public static void ClearAll()
-      {
+    public void Save()
+    {
         MySqlConnection conn = DB.Connection();
         conn.Open();
 
         var cmd = conn.CreateCommand() as MySqlCommand;
-        cmd.CommandText = @"DELETE FROM _database;";
-        cmd.ExecuteNonQuery();
+        cmd.CommandText = @"INSERT INTO stylists (sLastName, sFirstName) VALUES (@sLastName, @sFirstName);";
 
+        MySqlParameter description = new MySqlParameter();
+        description.ParameterName = "@sLastName";
+        sLastName.Value = this._sLastName;
+        cmd.Parameters.Add(sLastName);
+
+        MySqlParameter sFirstName = new MySqlParameter();
+        dueDate.ParameterName = "@sFirstName";
+        sFirstName.Value = this._sFirstName;
+        cmd.Parameters.Add(sFirstName);
+
+        cmd.ExecuteNonQuery();
+        _sId = (int) cmd.LastInsertedsId;
         conn.Close();
         if (conn != null)
         {
-          conn.Dispose();
+            conn.Dispose();
         }
-      }
+    }
+
+    // public static NewSalon
+    //
+    //     Salon newSalon = new Salon(SalonSLastName, SalonSFirstName, SalonId);
+    //
+    //     return false;
+
+    // public static void ClearAll()
+    //   {
+    //     MySqlConnection conn = DB.Connection();
+    //     conn.Open();
+    //
+    //     var cmd = conn.CreateCommand() as MySqlCommand;
+    //     cmd.CommandText = @"DELETE FROM _database;";
+    //     cmd.ExecuteNonQuery();
+    //
+    //     conn.Close();
+    //     if (conn != null)
+    //     {
+    //       conn.Dispose();
+    //     }
+    //   }
   }
 }
